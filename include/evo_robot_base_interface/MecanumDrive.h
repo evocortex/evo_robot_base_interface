15913adbb3a3d0883de151169ce3ecf823d6b242
Marco <masanator@hotmail.de>
2021-02-19 06:13:02 +0100

    part 2 of the kinematics extension

--- include/evo_robot_base_interface/MecanumDrive.h
+++ include/evo_robot_base_interface/MecanumDrive.h
@@ -23,167 +23,21 @@
 
 #include "evo_logger/log/Logger.h"
 
+#include "evo_robot_base_interface/2dKinematics.h"
+
 namespace evo {
-
-struct MecanumWheelData
+class MecanumDrive : public Drive2d
 {
-   double front_left = 0.0;
-   double front_right = 0.0;
-   double back_left = 0.0;
-   double back_right = 0.0;
-
-   inline MecanumWheelData& operator*=(const double factor)
-   {
-      this->front_left *= factor;
-      this->front_right *= factor;
-      this->back_left *= factor;
-      this->back_right *= factor;
-      return *this;
-   }
-};
-
-struct MecanumVel
-{
-   double _x_ms     = 0.0;
-   double _y_ms     = 0.0;
-   double _yaw_rads = 0.0;
-};
-
-struct MecanumPose
-{
-   double _x_m     = 0.0;
-   double _y_m     = 0.0;
-   double _yaw_rad = 0.0;
-
-   void reset()
-   {
-      // todo: test this line
-      *this = MecanumPose();
-   }
-
-   void updatePoseFromVel(const MecanumVel& vel, const double cycle_time)
-   {
-      this->_x_m += (std::cos(this->_yaw_rad) * vel._x_ms -
-                     std::sin(this->_yaw_rad) * vel._y_ms) *
-                    cycle_time;
-      this->_y_m += (std::sin(this->_yaw_rad) * vel._x_ms +
-                     std::cos(this->_yaw_rad) * vel._y_ms) *
-                    cycle_time;
-      this->_yaw_rad += vel._yaw_rads * cycle_time;
-   }
-
-   void updatePoseFromIncrement(const MecanumPose& inc)
-   {
-      this->_x_m +=  std::cos(this->_yaw_rad) * inc._x_m - 
-                     std::sin(this->_yaw_rad) * inc._y_m;
-      this->_y_m +=  std::sin(this->_yaw_rad) * inc._x_m + 
-                     std::cos(this->_yaw_rad) * inc._y_m;
-      this->_yaw_rad += inc._yaw_rad;
-   }
-};
-
-struct MecanumCovariance
-{
-   double cov_pos_x   = 0.0;
-   double cov_pos_y   = 0.0;
-   double cov_pos_yaw = 0.0;
-   double cov_vel_x   = 0.0;
-   double cov_vel_y   = 0.0;
-   double cov_vel_yaw = 0.0;
-};
-
-
-// MMA: split the class here? -> separation of mecanum maths and hw access
-// new class MecanumMaths or so
-// we should think about a software redesign (keep a lot of code but organize different)
-
-enum MOTOR_MAPPING_MECANUM
-{
-   NO_POSITION = 0,
-   FRONT_LEFT,
-   FRONT_RIGHT,
-   BACK_RIGHT,
-   BACK_LEFT
-};
-
-class MecanumDrive
-{
- private:
-   // references to motors of motor handler
-   std::shared_ptr<evo_mbed::Motor> _motor_front_left;
-   std::shared_ptr<evo_mbed::Motor> _motor_front_right;
-   std::shared_ptr<evo_mbed::Motor> _motor_back_left;
-   std::shared_ptr<evo_mbed::Motor> _motor_back_right;
-
-   // robot dimensions
-   double _wheel_radius_in_m;
-   double _wheel_separation_length_in_m;
-   double _wheel_separation_width_in_m;
-   double _wheel_separation_sum_in_m;
-
-   // helper values for calculations
-   double _ms2rpm;
-   double _rpm2ms;
-   double _rot2m;
-
-   std::string _logger_prefix;
-
-   bool _is_initialized;
-   bool _verbose;
-
-   MecanumWheelData _last_position;
-   MecanumWheelData _current_position;
-   MecanumWheelData _current_rpm;
-   //-------------
  public:
       MecanumDrive();
 
-    /**
-     * @brief checks if all motor references and robot dimensions are set
-     * 
-     * @return true if initialized correct 
-     */
-    bool checkInitState();
+   virtual void wheelData2OdomVel(const WheelData& wd, Vel2d& vel) override;
+   virtual void wheelData2OdomPose(const WheelData& wd, Pose2d& pose) override;
+   virtual void wheelData2OdomPoseInc(const WheelData& wd, WheelData& lwd, Pose2d& poin) override;
 
-    /**
-     * @brief Set the virtual Motor position for the mecanum drive
-     *        Extremly important to get this right, otherwise the kinematic model is incorrect!
-     * 
-     * @param motor - motor object from evo_mbed
-     * @param motor_mapping - see ENUM MOTOR_MAPPING_MECANUM for correct motor mapping
-     */
-    void setMotorRef(std::shared_ptr<evo_mbed::Motor> motor, const uint8_t motor_mapping);
+   // sending data
+   virtual void cmdVel2wheelData(const Vel2d& cmd_vel, WheelData& cmd_wd) override;
 
-    // Set the robot dimensions for this mecanum drive configuration
-    void setWheelRadiusInM(const double wheel_radius_in_m);
-    void setWheelSeparationLengthInM(const double wheel_separation_length_in_m);
-    void setWheelSeparationWidthInM(const double wheel_separation_Width_in_m);
-    void setWheelDistanceFrontBackInM(const double wheel_distance_front_back_in_m);
-    void setWheelDistanceLeftRightInM(const double wheel_distance_left_right_in_m);
-
-    // update all wheel data
-    bool resetEncoders(); 
-    bool readWheelData();
-
-    void wheelData2OdomVel(const MecanumWheelData& wd, MecanumVel& mv);
-    void wheelData2OdomPose(const MecanumWheelData& wd, MecanumPose& mp);
-    void wheelData2OdomPoseInc(const MecanumWheelData& wd, MecanumWheelData& lwd, MecanumPose& mpi);
-
-
-    bool getOdomComplete(MecanumVel& odom_vel, 
-                         MecanumPose& odom_pose_increment,
-                         MecanumWheelData& current_position,
-                         MecanumWheelData& current_velocity);
-
-    void cmdVel2wheelData(const MecanumVel& cmd_vel, MecanumWheelData& cmd_wd);
-
-    bool setCmdVel(const MecanumVel& cmd_vel);
-
-
-    // dont use this 
-    void debugMotorMapping();
-
-    void printWheelData(const MecanumWheelData& wd, const std::string& nametag);
 };
 } // namespace evo
 #endif // MECANUMDRIVE_H
