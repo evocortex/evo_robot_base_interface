90c698a5ee0e42c448b3631fdf9840d212828d55
Marco Masannek <masanator@hotmail.de>
2021-02-22 14:41:01 +0100

    part 3 of kinematics expansion

--- src/MecanumDrive.cpp
+++ src/MecanumDrive.cpp
@@ -20,44 +20,42 @@
 
 namespace evo {
 
-MecanumDrive::MecanumDrive() :
-    _logger_prefix("MecanumDrive: "), 
+MecanumDrive::MecanumDrive() : Drive2d()
 {
    evo::log::init("");
+   _logger_prefix = std::string("MecanumDrive: ");
 }
 
 
-
-
-void MecanumDrive::wheelData2OdomVel(const MecanumWheelData& wd, MecanumVel& mv)
+void MecanumDrive::wheelData2OdomVel(const WheelData& wd, Vel2d& vel)
 {
-   mv._x_ms = _rpm2ms * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
-   mv._y_ms = _rpm2ms * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
-   mv._yaw_rads = _rpm2ms * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
+   vel._x_ms = _rpm2ms * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
+   vel._y_ms = _rpm2ms * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
+   vel._yaw_rads = _rpm2ms * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
 }
 
-void MecanumDrive::wheelData2OdomPose(const MecanumWheelData& wd, MecanumPose& mp)
+void MecanumDrive::wheelData2OdomPose(const WheelData& wd, Pose2d& pose)
 {
-   mp._x_m = _rot2m * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
-   mp._y_m = _rot2m * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
-   mp._yaw_rad = _rot2m * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
+   pose._x_m = _rot2m * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
+   pose._y_m = _rot2m * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
+   pose._yaw_rad = _rot2m * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
 }
 
-void MecanumDrive::wheelData2OdomPoseInc(const MecanumWheelData& wd, MecanumWheelData& lwd, MecanumPose& mpi)
+void MecanumDrive::wheelData2OdomPoseInc(const WheelData& wd, WheelData& lwd, Pose2d& poin)
 {
-   MecanumWheelData inc_wd = wd;
+   WheelData inc_wd = wd;
    inc_wd.back_left -= lwd.back_left;
    inc_wd.back_right -= lwd.back_right;
    inc_wd.front_left -= lwd.front_left;
    inc_wd.front_right -= lwd.front_right;
 
-   wheelData2OdomPose(inc_wd, mpi);
+   wheelData2OdomPose(inc_wd, poin);
 
    lwd = wd;   
 }
 
 
-void MecanumDrive::cmdVel2wheelData(const MecanumVel& cmd_vel, MecanumWheelData& cmd_wd)
+void MecanumDrive::cmdVel2wheelData(const Vel2d& cmd_vel, WheelData& cmd_wd)
 {
    cmd_wd.front_left = _ms2rpm * (-cmd_vel._x_ms + cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
    cmd_wd.back_left  = _ms2rpm * (-cmd_vel._x_ms - cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
