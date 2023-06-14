361eb8ca6c87d7ef8d1e83c20b88ba8f651ba289
RBO <redouane.bouassaba@evocortex.com>
2023-06-14 14:29:35 +0200

    DAM: fix for motors mounted in inverted direction (calibration testbench evocortex)

--- src/MecanumDrive.cpp
+++ src/MecanumDrive.cpp
@@ -29,16 +29,16 @@ MecanumDrive::MecanumDrive() : Drive2d()
 
 void MecanumDrive::wheelData2OdomVel(const WheelData& wd, Vel2d& vel)
 {
-   vel._x_ms = _rpm2ms * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
-   vel._y_ms = _rpm2ms * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
-   vel._yaw_rads = _rpm2ms * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
+   vel._x_ms = -_rpm2ms * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
+   vel._y_ms = -_rpm2ms * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
+   vel._yaw_rads = -_rpm2ms * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
 }
 
 void MecanumDrive::wheelData2OdomPose(const WheelData& wd, Pose2d& pose)
 {
-   pose._x_m = _rot2m * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
-   pose._y_m = _rot2m * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
-   pose._yaw_rad = _rot2m * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
+   pose._x_m = -_rot2m * (-wd.front_left + wd.front_right - wd.back_left + wd.back_right) / 4.0;
+   pose._y_m = -_rot2m * ( wd.front_left + wd.front_right - wd.back_left - wd.back_right) / 4.0;
+   pose._yaw_rad = -_rot2m * (wd.front_left + wd.front_right + wd.back_left + wd.back_right) / (4.0 * _wheel_separation_sum_in_m);
 }
 
 void MecanumDrive::wheelData2OdomPoseInc(const WheelData& wd, WheelData& lwd, Pose2d& poin)
@@ -57,11 +57,11 @@ void MecanumDrive::wheelData2OdomPoseInc(const WheelData& wd, WheelData& lwd, Po
 
 void MecanumDrive::cmdVel2wheelData(const Vel2d& cmd_vel, WheelData& cmd_wd)
 {
-   cmd_wd.front_left = _ms2rpm * (-cmd_vel._x_ms + cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
-   cmd_wd.back_left  = _ms2rpm * (-cmd_vel._x_ms - cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
+   cmd_wd.front_left = -_ms2rpm * (-cmd_vel._x_ms + cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
+   cmd_wd.back_left  = -_ms2rpm * (-cmd_vel._x_ms - cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
 
-   cmd_wd.front_right =_ms2rpm * ( cmd_vel._x_ms + cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
-   cmd_wd.back_right = _ms2rpm * ( cmd_vel._x_ms - cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
+   cmd_wd.front_right =-_ms2rpm * ( cmd_vel._x_ms + cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
+   cmd_wd.back_right = -_ms2rpm * ( cmd_vel._x_ms - cmd_vel._y_ms + (_wheel_separation_sum_in_m * cmd_vel._yaw_rads));
 }
 
 } // namespace evo
